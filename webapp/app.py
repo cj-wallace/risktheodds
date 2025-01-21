244b94631d6b16d816e6ad3cea68460aaf64873c
Connor Wallace <47456687+cj-wallace@users.noreply.github.com>
2025-01-20 19:40:22 -0500

    Create new flask app to test POST commands for login / user creation

--- webapp/app.py
+++ webapp/app.py
@@ -1,40 +1,88 @@
-from flask import Flask
-import random
-from flask import request
-
+# app.py
+ 
+from flask import Flask, render_template, request, redirect, session
+import json
+import os
+ 
 app = Flask(__name__)
+ 
+# Set a secret key for encrypting session data
+users_file = os.path.join(os.path.dirname(__file__), 'users.json')
+app.secret_key = 'my_secret_key'
 
+def verify_user(username: str, password: str) -> bool:
+    users = {}
+    with open(users_file, 'r') as f:
+        users = json.load(f)
+    return username in users and users[username] == password
+
+def create_user(username: str, password: str) -> bool:
+    users = {}
+    with open(users_file, 'r') as f:
+        users = json.load(f)
+    if username in users:
+        return False
+    users[username] = password
+    with open(users_file, 'w') as f:
+        json.dump(users, f)
+    return True
+
+# To render a login form 
 @app.route('/')
-def index():
-    return 'Hello, World!'
+def view_form():
+    return render_template('login.html')
+ 
+# For handling get request form we can get
+# the form inputs value by using args attribute.
+# this values after submitting you will see in the urls.
+# e.g http://127.0.0.1:5000/handle_get?username=kunal&password=1234
+# this exploits our credentials so that's 
+# why developers prefer POST request.
+@app.route('/handle_get', methods=['GET'])
+def handle_get():
+    if request.method == 'GET':
+        username = request.args['username']
+        password = request.args['password']
+        print(username, password)
+        if username in users and users[username] == password:
+            return '<h1>Welcome!!!</h1>'
+        else:
+            return '<h1>invalid credentials!</h1>'
+    else:
+        return render_template('login.html')
 
-@app.route('/rand/', methods=['GET'])
-def get_rand(min=1, max=100):
-    try:
-        min = int(request.args.get('min', min))
-    except ValueError:
-        min = min
+@app.route('/handle_create', methods=['POST'])
+def handle_create():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if verify_user(username, password):
+            return '<h1>User already created!</h1>'
+        else:
+            created = create_user(username, password)
+            if (created):
+                return f'<h1>Created {username}!!!</h1>'
+            else:
+                return '<h1>User already created!</h1>'
+    else:
+        return render_template('login.html')
 
-    try:
-        max = int(request.args.get('max', max))
-    except ValueError:
-        max = max
-
-    # Get the random number
-    rand = random.randint(min, max)
-    return str(rand)
-
-@app.route('/updateScore', methods=['POST'])
-def update_score(current_score):
-    # Get the high score
-    high_score = 0
-
-    # Update the high score
-    high_score = max(high_score, current_score)
-
-    # TODO Save the high score to the database
-
-    return str(high_score)
-
+# For handling post request form we can get the form
+# inputs value by using POST attribute.
+# this values after submitting you will never see in the urls.
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if verify_user(username, password):
+            return f'<h1>Welcome {username}!!!</h1>'
+        else:
+            return '<h1>invalid credentials!</h1>'
+    else:
+        return render_template('login.html')
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
