52cffab8506356cd28e2aba18361b1b87990ec8e
Connor Wallace <47456687+cj-wallace@users.noreply.github.com>
2025-01-21 19:22:01 -0500

    Created MVP of risk game

--- webapp/app.py
+++ webapp/app.py
@@ -1,88 +1,82 @@
-# app.py
- 
 from flask import Flask, render_template, request, redirect, session
-import json
-import os
- 
+import random
+
 app = Flask(__name__)
- 
-# Set a secret key for encrypting session data
-users_file = os.path.join(os.path.dirname(__file__), 'users.json')
-app.secret_key = 'my_secret_key'
 
-def verify_user(username: str, password: str) -> bool:
-    users = {}
-    with open(users_file, 'r') as f:
-        users = json.load(f)
-    return username in users and users[username] == password
+score = 100.00
+history = []
 
-def create_user(username: str, password: str) -> bool:
-    users = {}
-    with open(users_file, 'r') as f:
-        users = json.load(f)
-    if username in users:
-        return False
-    users[username] = password
-    with open(users_file, 'w') as f:
-        json.dump(users, f)
-    return True
-
-# To render a login form 
-@app.route('/')
-def view_form():
-    return render_template('login.html')
- 
-# For handling get request form we can get
-# the form inputs value by using args attribute.
-# this values after submitting you will see in the urls.
-# e.g http://127.0.0.1:5000/handle_get?username=kunal&password=1234
-# this exploits our credentials so that's 
-# why developers prefer POST request.
-@app.route('/handle_get', methods=['GET'])
-def handle_get():
-    if request.method == 'GET':
-        username = request.args['username']
-        password = request.args['password']
-        print(username, password)
-        if username in users and users[username] == password:
-            return '<h1>Welcome!!!</h1>'
-        else:
-            return '<h1>invalid credentials!</h1>'
-    else:
-        return render_template('login.html')
-
-@app.route('/handle_create', methods=['POST'])
-def handle_create():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global score
+    global history
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        print(username, password)
-        if verify_user(username, password):
-            return '<h1>User already created!</h1>'
-        else:
-            created = create_user(username, password)
-            if (created):
-                return f'<h1>Created {username}!!!</h1>'
-            else:
-                return '<h1>User already created!</h1>'
-    else:
-        return render_template('login.html')
+        post_data = dict(request.form)
 
-# For handling post request form we can get the form
-# inputs value by using POST attribute.
-# this values after submitting you will never see in the urls.
-@app.route('/handle_post', methods=['POST'])
-def handle_post():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        print(username, password)
-        if verify_user(username, password):
-            return f'<h1>Welcome {username}!!!</h1>'
+        #Parse POST info and get random odds
+        risk = int(post_data['risk'])
+        wager = int(post_data['wager'])
+        winnings = round(((1 / ((100 - risk) / 100)) * wager) - wager, 2)
+        print(f"Risk: {risk} | Wager: {wager} | Winnings: {winnings}")
+        rand = int(get_rand())
+        win = bool(rand >= risk)
+        score_str = "{:.2f}".format(score)
+        winnings_str = "{:.2f}".format(winnings)
+        lose_str = "{:.2f}".format(wager)
+        if(win):
+            score += winnings
+            output = f"Score: {score_str} | Risk: {risk} | Rand: {rand} | Win: {winnings_str}"
+            message = f"You won ${winnings_str}!"
         else:
-            return '<h1>invalid credentials!</h1>'
-    else:
-        return render_template('login.html')
- 
+            score -= wager
+            output = f"Score: {score_str} | Risk: {risk} | Rand: {rand} | Loss: {lose_str}"
+            message = f"You lost ${lose_str}!"
+        
+        history.insert(0,output)
+        history = history[:5]
+
+        return render_template('risk.html',score=score_str, message=message, risk=int(risk), wager=int(wager), rand=int(rand), history=history)
+    elif request.method == 'GET':
+        return render_template('risk.html', wager=10, risk=50, score=score_str, history=history)
+
+@app.route('/scores', methods=['GET', 'POST'])
+def scores():
+    if request.method == 'POST':
+        post_data = dict(request.form)
+        score = float(post_data['score'])
+        return f"Score: {score}"
+    elif request.method == 'GET':
+        return "unfinished"
+
+
+
+@app.route('/rand/', methods=['GET'])
+def get_rand(min=1, max=100):
+    try:
+        min = int(request.args.get('min', min))
+    except ValueError:
+        min = min
+
+    try:
+        max = int(request.args.get('max', max))
+    except ValueError:
+        max = max
+
+    # Get the random number
+    rand = random.randint(min, max)
+    return str(rand)
+
+@app.route('/updateScore', methods=['POST'])
+def update_score(current_score):
+    # Get the high score
+    high_score = 0
+
+    # Update the high score
+    high_score = max(high_score, current_score)
+
+    # TODO Save the high score to the database
+
+    return str(high_score)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
